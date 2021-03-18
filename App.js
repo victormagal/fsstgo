2aa3f87ef738ee0c3537bf0dd56db8c414a64db5
Victor Magalhaes <victormagalhaes@MacBook-Pro-de-Victor.local>
2021-03-18 11:52:35 -0300

    beggining of the folders organization and react navigation

--- App.js
+++ App.js
@@ -81,7 +81,7 @@ const App: () => Node = () => {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next (essa fera meu - é o Faustão):
+            Read the docs to discover what to do next:
           </Section>
           <LearnMoreLinks />
         </View>
