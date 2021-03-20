e6e9538a48ec6b259c07b5d4893bd4c5a52bfd99
Victor Magalhaes <victormagalhaes@MacBook-Pro-de-Victor.local>
2021-03-19 23:31:28 -0300

    svg libraries inserted in the project and home page already with some styles

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,14 @@
 import styled from 'styled-components/native';
 
-export const Title = styled.Text`
-    font-size: 30;
+export const Card = styled.TouchableOpacity`
+    background-color: #56B597;
+    border-radius: 10px;
+    margin: 0px 16px 16px;
+    padding: 16px;
+`;
+
+export const Label = styled.Text`
+    font-size: 22px;
+    line-height: 24px;
     color: black;
 `;
