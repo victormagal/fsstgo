2cc3634c6643ff90e633956913fe33690ebea2c9
Antonio Junior <juniordsj@gmail.com>
2021-03-25 21:05:15 -0300

    feat: ajuste statusbar ios e android e alterado arquivo App.js

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Faq from './pages/Faq';
+import Login from './pages/Login';
+// import Home from './pages/Home';
+// import Profile from './pages/Profile';
+// import Faq from './pages/Faq';
 
 export default function Routes() {
-    return (
-        <Stack.Navigator>
-            {/* <Stack.Screen name="Home" component={Home} /> */}
-            <Stack.Screen name="Profile" component={Profile} />
-            {/* <Stack.Screen name="Faq" component={Faq} /> */}
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      {/* <Stack.Screen name="Home" component={Home} /> */}
+      {/* <Stack.Screen name="Profile" component={Profile} /> */}
+      {/* <Stack.Screen name="Faq" component={Faq} /> */}
+    </Stack.Navigator>
+  );
 }
